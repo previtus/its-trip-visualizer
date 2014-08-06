82b4ec8f4154f77d3c0c42b1d06304e9d51b5f8a
previtus <previtus@gmail.com>
2014-08-06 19:11:57 +0200

    WAR deployment version
    however WAR manager is currently borked (even for older versions) so we
    are DOOOMED...

--- TripVisualizer/web/helperFunc.js
+++ TripVisualizer/web/helperFunc.js
@@ -18,7 +18,7 @@ function capitaliseFirstLetter(string)
 String.prototype.replaceAll = function(str1, str2, ignore) 
 {
 	return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
-}
+} 
 function makeLowerCase(value) {
   return value.toString().toLowerCase();
 }
